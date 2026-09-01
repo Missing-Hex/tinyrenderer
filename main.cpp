6f65defda13cc136a9a0abf427490c48147c9813
Missing-Hex <1304266750@qq.com>
2026-09-01 16:19:04 +0800

    barycentric coords of color

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
     return .5*((by-ay)*(bx+ax) + (cy-by)*(cx+bx) + (ay-cy)*(ax+cx));
 }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+void triangle(int ax, int ay, int az,  int bx, int by, int bz, int cx, int cy, int cz, TGAImage &framebuffer) {
     int bbminx = std::min(std::min(ax, bx), cx); // bounding box for the triangle
     int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
     int bbmaxx = std::max(std::max(ax, bx), cx);
@@ -57,7 +57,8 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
             double beta  = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha<0 || beta<0 || gamma<0) continue; // negative barycentric coordinate => the pixel is outside the triangle
-            framebuffer.set(x, y, color);
+            unsigned char z = static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
+            framebuffer.set(x, y, {z});
         }
     }
 }
@@ -68,6 +69,7 @@ std::tuple<int,int> project(vec3 v) { // First of all, (x,y) is an orthogonal pr
 }
 
 int main(int argc, char** argv) {
+    /*
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
         return 1;
@@ -84,6 +86,18 @@ int main(int argc, char** argv) {
         for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
         triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
     }
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    */
+    constexpr int width  = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::GRAYSCALE);
+
+    int ax = 17, ay =  4, az =  13;
+    int bx = 55, by = 39, bz = 128;
+    int cx = 23, cy = 59, cz = 255;
+
+    triangle(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
